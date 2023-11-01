e4eea46af837fc870e49475e8a3accaad1ee3f42
Rishabh Dwivedi <rishabh.dwivedi34@gmail.com>
2023-11-01 16:42:58 +0530

    First package

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Copyright © Magento All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Practice_PackageComponent', __DIR__);
